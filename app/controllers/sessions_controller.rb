00a0651bd33a22cacbefb09c394b66bb8d6fed88
Sonu <er.sonukr@gmail.com>
2020-04-21 00:56:14 +0530

    Session created.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    outcome = Auth::Login.run(username: params[:email], password: params[:password])
+    if outcome.valid?
+      session[:user_email] = params[:email]
+      session[:user_token] = outcome.result.dig('data', 'token', 'access_token')
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      flash[:alert] = 'Oops! Unable to login'
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    Auth::Logout.run(token: session[:user_token])
+    reset_session
+    flash[:notice] = 'Logged out!'
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => "widgets#index"
+  resource :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create] do
+    collection do
+      post :reset_password
+      get  :widgets
+    end
+  end
+  resources :widgets, only: [:index, :create]
 end
