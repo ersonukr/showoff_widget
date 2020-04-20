259fa3544450fbbba48fe778ced3b658468c2755
Sonu <er.sonukr@gmail.com>
2020-04-21 00:03:44 +0530

    Flash messages included.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :info, :error, :warning
+  helper_method :current_user
+
+  private
+  def current_user
+    session[:user_token]
+  end
+
 end
